2ab7a3858178ba9bcc8df32fac96fa015768f7eb
amarten <alexdmarten@gmail.com>
2022-02-05 16:09:07 -0800

    routes for products and tags added for updating, deleting and creating

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -146,6 +146,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    if(!data) {
+      res.status(404).json({ message: 'No Products found with this ID!' });
+      return;
+    }
+    res.json(data)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
